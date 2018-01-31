721de3460640310d32a99c047188342246f6a014
Tamara Hall <31778499+Highlyne@users.noreply.github.com>
2018-01-31 17:02:08 -0500

    Update apiRoutes.js
    setting things back to 11am

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,33 +7,21 @@
 // Requiring the model
 var db = require("../models");
 var pageController = require("../controllers/pageView");
-
 // Routes
 // =============================================================
 module.exports = function(app) {
     // GET route for getting all of the todos
     app.get("/api/results/:name", function(req, res) {
-    console.log("Parameter name: " + req.params.name)
+    console.log("Parameter name: "+ req.params.name)
     // findAll returns all entries for a table when used with no options
         db.MP.findAll({
             where:{
                 name: req.params.name
             }
-        }).then(function(data){
-            res.render('results', {MisPer: data})
-        });
-    });
-            // where:{
-            //     name: req.params.name
-            // }
-        // }).then(function(dbMP) {
+        }).then(function(dbMP) {
       // We have access to the todos as an argument inside of the callback function
-        //  res.json(dbMP);
-        // pageController.renderSearch();
-        // });
-       
-    
-    // )
-    // app.get("/api/results", function(req, res) {
-    //     res.render('results', {MisPer:dbMP})})
+        res.json(dbMP);
+        pageController.renderResults(dbMP);
+    });
+  })
 };
